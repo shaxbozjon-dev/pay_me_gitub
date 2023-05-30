b9e83508c6859cc6229d48d6482f5bdd760fbf05
username <email>
2023-05-31 00:03:05 +0500

    hel

--- lib/services/io_servise.dart
+++ lib/services/io_servise.dart
@@ -1,1 +1,18 @@
+import 'dart:io';
 //IO service yozin yoki eskisini copy qilin
+
+class IoServise {
+  void inputText(String str) {
+    stdout.write(str);
+  }
+
+  String inpudText(String str) {
+    inputText(str);
+    return stdin.readLineSync()!;
+  }
+
+  int inpudNumber(String a) {
+    inputText(a);
+    return int.parse(stdin.readLineSync()!);
+  }
+}
