64fad3e0df4a899e3ebb34a46c9075ed96f551b7
username <email>
2023-05-30 22:33:20 +0500

    Payme project

---
+++ bin/pay_me_gitub.dart
@@ -0,0 +1,5 @@
+import 'package:pay_me_gitub/pay_me_gitub.dart' as pay_me_gitub;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pay_me_gitub.calculate()}!');
+}
---
+++ test/pay_me_gitub_test.dart
@@ -0,0 +1,8 @@
+import 'package:pay_me_gitub/pay_me_gitub.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
