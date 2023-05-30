10beaa0947281b6b507676ac45f9752c41d4a495
username <email>
2023-05-30 23:36:23 +0500

    hello

--- bin/pay_me_gitub.dart
+++ bin/pay_me_gitub.dart
@@ -1,5 +1,7 @@
 import 'package:pay_me_gitub/pay_me_gitub.dart' as pay_me_gitub;
 
 void main(List<String> arguments) {
+  print(
+      "_______________________________PayMe ga Xush kelibsiz___________________\n");
   print("Endi biza bitta koddi ikkalamiz ishlatolimiz!!!");
 }
