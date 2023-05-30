3af15bd580fac4808c098a5364b551e60a17bf6b
username <email>
2023-05-30 23:21:33 +0500

    uxshagandir

--- bin/pay_me_gitub.dart
+++ bin/pay_me_gitub.dart
@@ -1,5 +1,6 @@
 import 'package:pay_me_gitub/pay_me_gitub.dart' as pay_me_gitub;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pay_me_gitub.calculate()}!');
+  print(
+      'Mening birinchi githubdagi proekt uxshadimi: ${pay_me_gitub.calculate()}!');
 }
