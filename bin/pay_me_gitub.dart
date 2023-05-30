a5de2acae14956178ef00fd06badef90bd2eca32
erkinoff <murodjonerkinov2005@gmail.com>
2023-05-30 23:27:42 +0500

    .

--- bin/pay_me_gitub.dart
+++ bin/pay_me_gitub.dart
@@ -1,6 +1,5 @@
 import 'package:pay_me_gitub/pay_me_gitub.dart' as pay_me_gitub;
 
 void main(List<String> arguments) {
-  print(
-      'Mening birinchi githubdagi proekt uxshadimi: ${pay_me_gitub.calculate()}!');
+  print("Endi biza bitta koddi ikkalamiz ishlatolimiz!!!");
 }
