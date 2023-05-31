c7a85678592f39974e8c3bad4cb54fc7dd626277
username <email>
2023-05-31 21:23:09 +0500

    qushdim

--- lib/menu/menu_1.dart
+++ lib/menu/menu_1.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import '../services/io_servise.dart';
 import '../models/user.dart';
 import '../data_base/data_base.dart';
@@ -17,13 +19,20 @@ class Menu {
     for (var e in data.dataBase.entries) {
       if (e.key == number) {
         String parol = io.inpudText("parol kiriting: ");
+        if (e.value == parol) {
+          print(e);
+        }
       } else {
         print("Ruyhatdan utasizimi😎\n1.ha\n2.yuq");
 
         String str = io.inpudText("1.ha\n2.yuq");
         switch (str) {
           case "1":
-            print("ruyhatdan uting");
+            String number = io.inpudText("number kiriting: ");
+            String password = io.inpudText("pasword kiriting: ");
+
+            User newuser = User(number: number, password: password);
+
             break;
           case "2":
             print("ortga qaytdik");
