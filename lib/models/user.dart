4339273b1ed56a5fec8dd5cfb160601d3f196fc9
username <email>
2023-05-31 20:26:08 +0500

    hello

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,33 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 ///User klasi har bir userni ma'lumotlarini saqlaydi
 class User {
   String number;
   String password;
+  List? cards;
 
   User({
     required this.number,
     required this.password,
+    this.cards,
   });
 //^Gitgub da ishlash zur ekanku😎😎✅
   // * User o'zida bir nechta kartalarni saqlashi mumkin.
   // * Masalan sizda ham 2 yoki 3 ta karta bo'lishi mumkin digande
   // * Shunichun cards digan list ochib olish kerak. Ichida Card lar bo'ladi
 
-  //! bizga copyWith kerakmas chunki userni copy qiladigan narsasi yo'q
-  // User copyWith({
-  //   String? number,
-  //   String? password,
-  // }) {
-  //   return User(
-  //     number: number ?? this.number,
-  //     password: password ?? this.password,
-  //   );
-  // }
-
   ///User obyektini Mapga o'girishimiz kerak bo'ladi shuning uchun toMap kerak
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'number': number,
       'password': password,
+      'List': cards,
     };
   }
 
@@ -36,28 +29,7 @@ class User {
     return User(
       number: map['number'] as String,
       password: map['password'] as String,
+      cards: map['List'] as List,
     );
   }
-
-  //! Bizga hozir toJson va fromJson kerakmas
-  // String toJson() => json.encode(toMap());
-
-  // factory User.fromJson(String source) =>
-  //     User.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  //! toString ham kerakmas chunki biz userni paroli bilan birga hich qachon chiqarmaymmiz
-  // @override
-  // String toString() => 'User(number: $number, password: $password)';
-
-  //! Ikkita userni solishtirish ham kerak bo'lmaydi
-  // @override
-  // bool operator ==(covariant User other) {
-  //   if (identical(this, other)) return true;
-
-  //   return other.number == number && other.password == password;
-  // }
-
-  //! Bu ham
-  // @override
-  // int get hashCode => number.hashCode ^ password.hashCode;
 }
