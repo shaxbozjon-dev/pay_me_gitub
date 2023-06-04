dbfd84cbe5af33989abfb6b8d6ce1a9fc7f0517c
shaxbozjon215@gmail.com <email>
2023-06-04 21:42:25 +0500

    salom

--- lib/data_base/data_base.dart
+++ lib/data_base/data_base.dart
@@ -2,18 +2,17 @@ import 'package:pay_me_gitub/models/card.dart';
 
 import '../models/user.dart';
 
-Card card =
-    Card(name: "karta", number: "1234", balance: "123", password: "123123");
-Map<String, Card> dataBase = {
-  "1": card,
-  
+import '../models/user.dart';
+
+Map<String, String> dataBase = {
+  "1": "1",
 };
 List bazacards = [];
 bool createData({required User user}) {
   if (dataBase.containsKey(user.number)) {
     return false;
   } else {
-    dataBase[user.number] = user.toJson() as Card;
+    dataBase[user.number] = user.toJson();
     return true;
   }
 }
--- lib/menu/menu_1.dart
+++ lib/menu/menu_1.dart
@@ -7,25 +7,22 @@ import '../services/io_servise.dart';
 
 void menu2() {
   print("Hush kelibsiz ");
-  String number = io.inpudText("number kiriting: ");//raqam kiritishni suraydi
+  String number = io.inpudText("number kiriting: ");
 
   print(dataBase);
 
-  if (dataBase.containsKey(number.trim())) {//agar databaseda biza kiritgan raqam bormi deb tekshiradi
-    var data = dataBase[number];//agar biza kiritgan raqam bulsa  shuni yangi uzgaruvchiga uzlashtiradi
+  if (dataBase.containsKey(number.trim())) {
+    var data = dataBase[number];
   
     print("ruyhada bor ekansiz😎");
-    String parol = io.inpudText("parol kiriting: ");//number ruyhatda bulgani uchun parol suraydi
-    if (dataBase[parol] == parol) {//agar parol tugri bulsa menuga kiradi
+    String parol = io.inpudText("parol kiriting: ");
+    if (data == parol) {
       
       String commmon = io.inpudText(
-          "1.Add card\n2.My cards\n3.Balance\n4.Utkazmalar\n5.settings\n0.ortga");
+          "1.Add card\n2.My cards3.\nBalance\n4.Utkazmalar\n5.settings\n0.ortga");
       switch (commmon) {
         case "1":
-        
-// data.
-
-          // addcards();
+          addcards();
         case "2":
           //MyCards
           break;
@@ -34,19 +31,19 @@ void menu2() {
 
       //Menu
     } else {
-      print("number yoki parol xato");//agar parol xato terilgan
+      print("number yoki parol xato");
     }
   } else {
-    print("Ruyhatdan utasizimi😎");//ruyhatdan bulmagani uchun ,ruyhattdan utishni taklif qilyabdi
+    print("Ruyhatdan utasizimi😎");
 
     String str = io.inpudText("1.ha\n2.yuq\n--> ");
     switch (str) {
-      case "1"://endi ism familiya suraydi
+      case "1":
         String number = io.inpudText("number kiriting: ");
         String password = io.inpudText("pasword kiriting: ");
 
-        User newuser = User(number: number, password: password, cards: []);//yangi user yasadik
-        dataBase.addEntries([MapEntry(newuser.number, {newuser.password:newuser.cards} as Card)]);//yangi userni map kurinishida datbasega saqlaymiz
+        User newuser = User(number: number, password: password, cards: []);
+        dataBase.addEntries([MapEntry(newuser.number, newuser.password)]);
         print(dataBase);
 
         break;
